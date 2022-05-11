b816d7ac8ad8862a66ee7605281e5938c04bff93
GuDantas <gustavo.em.dantas@gmail.com>
2022-05-11 18:50:38 -0300

    third try, this should work

---
+++ jokenpo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace jokenpo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolhaJogador, escolhaComputador;
+            Console.WriteLine("***************************");
+            Console.WriteLine("******* J O K E N P O *****");
+            Console.WriteLine("***************************");
+            Console.WriteLine();
+            Console.WriteLine("OPÇÔES DO JOGO");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+            Console.WriteLine("[1] - Pedra");
+            Console.WriteLine("[2] - Papel");
+            Console.WriteLine("[3] - Tesoura");
+
+            escolhaJogador = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            escolhaComputador = random.Next(1, 4);
+            
+            if (escolhaComputador == escolhaComputador)
+            {
+                Console.WriteLine("EMPATE");
+            }
+        }
+    }
+}
