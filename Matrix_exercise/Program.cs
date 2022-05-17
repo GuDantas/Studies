598914c11ee2c2750b608aeeeccda382e67d6b89
GuDantas <gustavo.em.dantas@gmail.com>
2022-05-16 22:06:16 -0300

    monday class update

---
+++ Matrix_exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Matrix_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] Matrix = new double[5, 5];
+            for (int line = 0; line < 5; line++)
+            {
+                Matrix[line, 0] = line + 1;
+                Console.WriteLine("Student {0}", line + 1);
+                for(int coluna = 1; coluna < 5; coluna++)
+                {
+                    Console.WriteLine("Type the grade from the first bimester {0}",coluna);
+                    Matrix[line, coluna] = Convert.ToDouble(Console.ReadLine());
+                }
+            } 
+        }
+    }
+}
