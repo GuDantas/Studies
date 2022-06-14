4a92d10b07dd95b5ceb610681a988ce33e3b8c1f
GuDantas <gustavo.em.dantas@gmail.com>
2022-06-14 14:26:12 -0300

    Doing the exercise with minor changes

---
+++ Module_2/OOP_Academy/Class2_baking_app/Exceptions/InsufficientBalanceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2_banking_app.Exceptions
+{
+    class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
